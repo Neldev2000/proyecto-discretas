82e964c3ae78979afa632baccf234df015372c7c
Nelson Vivas <nelsonandresvivaslobo@gmail.com>
2024-05-06 22:07:41 -0400

    Agregado Video

--- src/routes/clases/encriptacion/+page.svelte
+++ src/routes/clases/encriptacion/+page.svelte
@@ -9,7 +9,6 @@
 	<h1 class="font-bold text-4xl mt-20">Temas</h1>
 
 	<div class="mt-10 gap-x-40 mr-20">
-		
 		<p>
 			Para poder comprender el encriptado y sus algoritmos, primero debemos conocer las motivaciones
 			que nos han llevado a crear esta herramienta con infinidad de beneficios en nuestra
@@ -17,10 +16,27 @@
 			<br /><br />
 			La necesidad del ser humano de resguardar informacion importante para que otras personas no la
 			vean llevo a Militares, Matematicos e Ingenieros a romper las barreras del lenguaje comun y crear
-			mensajes ocultos de tal manera que solo un selecto grupo con conocimiento
-			del como se estructuran esos mensajes puedan interpretarlos
-            <br /><br />
-            Asi fue el caso de Julio Cesar,
+			mensajes ocultos de tal manera que solo un selecto grupo con conocimiento del como se estructuran
+			esos mensajes puedan interpretarlos
+			<br /><br />
+			Asi fue el caso de Julio Cesar, quien al estar en constantes campañas militares necesitaba comunicarse 
+            con sus generales a kilometros de distancia por lo que empleo un metodo de encriptacion novedoso conocido
+            hoy en dia como el metodo de Cesar.
 		</p>
+
+        
+
+		<div class="video-container w-full ">
+			<iframe
+				width="560"
+				height="315"
+                class="rounded-lg w-1/2"
+				src="https://www.youtube.com/embed/ttqI7EFW3Mk"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+				allowfullscreen
+				title="Metodo de Cesar"
+			></iframe>
+		</div>
 	</div>
 </div>
