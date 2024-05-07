72fd2d42e8a312518f2684f4201fde078ad369db
Nelson Vivas <nelsonandresvivaslobo@gmail.com>
2024-05-07 13:44:46 -0400

    Cambiado estilos

--- src/lib/main-page/componentes/CardClases.svelte
+++ src/lib/main-page/componentes/CardClases.svelte
@@ -2,4 +2,4 @@
     export let  enlace: string, titulo: string;
 </script>
 
-<a href={enlace} class="bg-second text-primary text-xl font-bold p-4 rounded-2xl"> {titulo}</a>
+<a href={enlace} class="bg-second text-backgroundColor text-xl font-bold p-4 rounded-2xl"> {titulo}</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,22 +3,24 @@
 	import { clases } from '$lib/main-page/utils/clases';
 </script>
 
-<div class="ml-40 mt-40">
-	<h1 class="font-bold text-5xl text-second">Code Nook</h1>
-	<h1 class="text-3xl mt-10 text-second">
-		Tu lugar favorito para aprender de Teoria de <br /> Codificacion
-	</h1>
-	<h1 class="font-bold text-4xl mt-20 text-second">Temas</h1>
 
-	<div class="mt-20 flex justify-center items-center gap-x-40">
-		{#each clases as clase}
-			<CardClases enlace={clase.enlace} titulo={clase.titulo} />
-		{/each}
+<html lang="en">
+	<div class="ml-40 mt-40">
+		<h1 class="font-bold text-5xl text-second">Code Nook</h1>
+		<h1 class="text-3xl mt-10 text-second">
+			Tu lugar favorito para aprender de Teoria de <br /> Codificacion
+		</h1>
+		<h1 class="font-bold text-4xl mt-20 text-second">Temas</h1>
+	
+		<div class="mt-20 flex justify-center items-center gap-x-40">
+			{#each clases as clase}
+				<CardClases enlace={clase.enlace} titulo={clase.titulo} />
+			{/each}
+		</div>
 	</div>
-</div>
-
+</html>
 <style lang="postcss">
 	:global(html) {
-		background-color: theme(colors.primary);
+		background-color: theme(colors.backgroundColor);
 	}
 </style>
