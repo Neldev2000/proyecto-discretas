958c4935f0f9b17fc5d892b8a698736106998668
Nelson Vivas <nelsonandresvivaslobo@gmail.com>
2024-05-09 19:02:06 -0400

    RSA CARGADO

---
+++ src/lib/clases/encriptacion/SeccionRSA.svelte
@@ -0,0 +1,35 @@
+<div class="text-lg">
+    <p>
+        Con el avance de la tecnologia y la computacion, todos estos metodos han quedado poco a poco obsoletos,
+        pues, esta nueva herramienta nos permite descifrar mensajes en cuestion de segundos. Por lo que investigadores
+        empezaron a buscar nuevas formas de encriptar mensajes y asi nacio el algoritmo RSA. 
+        <br><br>
+        Para comprender RSA, debemos recordar que en el cifrado de  Vigenere utilizabamos una llave privada para
+        cifrar y descifrar mensajes, pero esta llave las unicas personas que tenian conocimiento de esto era el emisor
+        y el receptor. Pero RSA va mas alla, pues, utiliza dos llaves, una publica y una privada. Es decir, un codigo
+        que todo el mundo puede conocer sin miedo a que alguien pueda descifrar el mensaje. Y otra llave que solo el
+        receptor conoce, para descifrar el mensaje.
+        <br><br> 
+        El algoritmo RSA se basa en la factorizacion de numeros primos, es decir, se eligen dos numeros primos p y q muy
+        grandes y se multiplican, este nuevo numero va a ser la llave publica, y tu llave privada van a ser estos numeros
+        p y q. Resulta que hallar estos nuevos numeros a partir de la llave publica es muy dificil (una computadora
+        pudiese tardar miles de años en hacerlo), pero si tienes la llave privada, es muy facil. 
+    </p>
+    <p>A continuacion se mostrara un video explicativo del metodo RSA</p>
+    
+    <p class="font-bold text-2xl my-10">Video Explicativo</p>
+	<div class="video-container w-full flex justify-center">
+		<iframe
+			width="560"
+			height="315"
+			class="rounded-lg w-1/2"
+			src="https://www.youtube.com/embed/Q8K311s7EiM"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowfullscreen
+			title="Metodo de RSA"
+		></iframe>
+	</div>
+
+    
+</div>
--- src/lib/clases/encriptacion/VigenereMensajes.svelte
+++ src/lib/clases/encriptacion/VigenereMensajes.svelte
@@ -1,52 +1,64 @@
 <script lang="ts">
-    import { vigenereCipher, generateVigenereKey } from '$lib/clases/encriptacion/utils'; // Asegúrate de que la ruta y los nombres de las funciones sean correctos
-    import Icon from '@iconify/svelte';
+	import { vigenereCipher, generateVigenereKey } from '$lib/clases/encriptacion/utils'; // Asegúrate de que la ruta y los nombres de las funciones sean correctos
+	import Icon from '@iconify/svelte';
 
-    export let llave, mensaje;
-    let vigenereKey:string;
-    let cifrado:string;
-    $: {
-        vigenereKey = generateVigenereKey(mensaje.toUpperCase(), llave.toUpperCase());
-         cifrado = vigenereCipher(mensaje.toUpperCase(), vigenereKey);
-    }
+	export let llave, mensaje;
+	let vigenereKey: string;
+	let cifrado: string;
+	$: {
+		vigenereKey = generateVigenereKey(mensaje.toUpperCase(), llave.toUpperCase());
+		cifrado = vigenereCipher(mensaje.toUpperCase(), vigenereKey);
+	}
 </script>
 
 <div class="flex flex-col gap-y-1 justify-center items-center">
-    <div class="">
-        <p>Aqui veras el proceso para generar el mensaje cifrado, el texto en <strong class="text-green">Verde</strong> 
-            es tu mensaje original, el texto en <strong class="text-red">Rojo</strong> es tu llave secreta y el texto en 
-            <strong class="text-blue">Azul</strong> es tu mensaje cifrado.
-        </p>
-    </div>
-    <div class="flex justify-center items-center ">
-        {#each Array.from(mensaje.toUpperCase()) as letra}
-        <div class=" p-4 border-2 bg-green text-white font-bold rounded-lg border-black mx-1 w-6 h-6 flex justify-center items-center">{letra}</div>
-        {/each}
-    </div>
-    <div class="flex justify-center items-center ">
-        {#each Array.from(mensaje) as _}
-
-                    <Icon icon="mingcute:arrows-down-fill" class=" mx-1 w-8 h-6 " />
-
-
-        {/each}
-    </div>
-    <div class="flex justify-center items-center ">
-        {#each Array.from(vigenereKey) as letra}
-        <div class="p-4 border-2 rounded-lg bg-red text-white font-bold border-black mx-1 w-6 h-6 flex justify-center items-center">{letra}</div>
-        {/each}
-    </div>
-    <div class="flex justify-center items-center ">
-        {#each Array.from(mensaje) as _}
-
-                    <Icon icon="mingcute:arrows-down-fill" class=" mx-1 w-8 h-6 " />
-
-
-        {/each}
-    </div>
-    <div class="flex justify-center items-center ">
-        {#each Array.from(cifrado) as letra}
-        <div class="p-4 border-2 rounded-lg bg-blue text-white font-bold border-black mx-1 w-6 h-6 flex justify-center items-center">{letra}</div>
-        {/each}
-    </div>
+	<div class="">
+		<p>
+			Aqui veras el proceso para generar el mensaje cifrado, el texto en <strong class="text-green"
+				>Verde</strong
+			>
+			es tu mensaje original, el texto en <strong class="text-red">Rojo</strong> es tu llave secreta
+			y el texto en
+			<strong class="text-blue">Azul</strong> es tu mensaje cifrado.
+		</p>
+	</div>
+	{#if llave.length > 0}
+		<div class="flex justify-center items-center">
+			{#each Array.from(mensaje.toUpperCase()) as letra}
+				<div
+					class=" p-4 border-2 bg-green text-white font-bold rounded-lg border-black mx-1 w-6 h-6 flex justify-center items-center"
+				>
+					{letra}
+				</div>
+			{/each}
+		</div>
+		<div class="flex justify-center items-center">
+			{#each Array.from(mensaje) as _}
+				<Icon icon="mingcute:arrows-down-fill" class=" mx-1 w-8 h-6 " />
+			{/each}
+		</div>
+		<div class="flex justify-center items-center">
+			{#each Array.from(vigenereKey) as letra}
+				<div
+					class="p-4 border-2 rounded-lg bg-red text-white font-bold border-black mx-1 w-6 h-6 flex justify-center items-center"
+				>
+					{letra}
+				</div>
+			{/each}
+		</div>
+		<div class="flex justify-center items-center">
+			{#each Array.from(mensaje) as _}
+				<Icon icon="mingcute:arrows-down-fill" class=" mx-1 w-8 h-6 " />
+			{/each}
+		</div>
+		<div class="flex justify-center items-center">
+			{#each Array.from(cifrado) as letra}
+				<div
+					class="p-4 border-2 rounded-lg bg-blue text-white font-bold border-black mx-1 w-6 h-6 flex justify-center items-center"
+				>
+					{letra}
+				</div>
+			{/each}
+		</div>
+	{/if}
 </div>
